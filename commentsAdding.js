aef0f2cdc9aa9ab877f16d17d9c065b47088e65a
Jozef Sedlák <jozef.sedlak.3@student.tuke.sk>
2020-12-01 13:58:37 +0100

    pridana funkcionalita na tlacidlo pokracovat(zatial len alert) a pridane nejake importy na firebase databazu, ale nefunkcne

--- commentsAdding.js
+++ commentsAdding.js
@@ -1,5 +1,40 @@
+//The core Firebase JS SDK is always required and must be listed first
+//include('https://www.gstatic.com/firebasejs/8.1.1/firebase-analytics.js');
+//TODO: Add SDKs for Firebase products that you want to use
+//https://firebase.google.com/docs/web/setup#available-libraries
+//include('https://www.gstatic.com/firebasejs/8.1.1/firebase-firestore.js');
+//include('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+
+//import * as firebase from "firebase/app";
+//import 'firebase/firestore';
 
 
+let selectedString = '';
+let inputString = '';
+//const firebase = window.firebase;
+    //require("firebase/firestore");
+//const firebase = require("firebase/app");
+// Required for side-effects
+
+/*
+let firebaseConfig = {
+    apiKey: "AIzaSyA8S_DJAlXAZfvKDvm5g1fC53lvxJ1Rq1Y",
+    authDomain: "hackathon-2020-1a7e6.firebaseapp.com",
+    databaseURL: "https://hackathon-2020-1a7e6.firebaseio.com",
+    projectId: "hackathon-2020-1a7e6",
+    storageBucket: "hackathon-2020-1a7e6.appspot.com",
+    messagingSenderId: "619297380907",
+    appId: "1:619297380907:web:f80b8b6e8bd2b9722c286e",
+    measurementId: "G-FY877D66KG"
+};
+// Initialize Firebase
+//firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+//firebase.analytics();
+
+
+ */
 
 //ziska vybraty text
 function getSelectedText() {
@@ -17,6 +52,31 @@ function getSelectedText() {
     } else return '';
     return selectedText;
 }
+/*
+function addCorrection() {
+    if (selectedString!=='' && inputString!== '') {
+        db.collection("selected").add({
+            selected: selectedString,
+            changeTo: inputString
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+    }
+    else {
+        alert('There is no selected text or input field is empty.');
+    }
+}
+
+ */
+
+function addCorrection(inputText) {
+    alert('added input string: ' + inputText);
+}
+
 
 function createTextField(fromTop) {
     let textDiv = document.createElement("div");
@@ -24,6 +84,7 @@ function createTextField(fromTop) {
     let continueButton = document.createElement("button");
     let inputTextField = document.createElement("INPUT");
     inputTextField.setAttribute("type", "text");
+    inputTextField.value = "";
     textDiv.style.width = "10%";
     textDiv.style.right = "10%";
     textDiv.style.border = "solid black 1px";
@@ -31,7 +92,12 @@ function createTextField(fromTop) {
     textDiv.style.position = "absolute";
     textDiv.style.backgroundColor = "yellow";
     textDiv.style.top = fromTop + "px";
+
     continueButton.innerHTML = "Pokračuj";
+    continueButton.onclick = function() {
+        let inputText = inputTextField.value;
+        addCorrection(inputText);
+    };
     cancelButton.innerHTML = "Zruš";
     cancelButton.onclick = function() {
         textDiv.removeChild(cancelButton);
@@ -94,8 +160,8 @@ let markSelection = (function() {
 
 //pri zmene vybrateho textu vezme text a vytvori tlacidlo
 document.onselectionchange = () => {
-    let text = getSelectedText();
-    if (text!=='') {
+    let selectedText = getSelectedText();
+    if (selectedText!=='') {
         markSelection(window);
     }
 };
